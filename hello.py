33479951227e743bc9bf5b9c19a1ea76cefc9d2d
Yu Xin Cheng <yx.cheng73@gmail.com>
2021-09-25 17:13:46 -0400

    Completed Activity 3 to include navbar, hello name, and current time

--- hello.py
+++ hello.py
@@ -1,13 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template
+from datetime import datetime
+
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 @app.route('/')
 def index():
-    return '<h1>Hello World!</h1>'
+    return render_template('index.html')
     
 @app.route('/user/<name>')
 def user(name):
-    return '<h1>Hello, %s!</h1>' % name
+    print(datetime.utcnow())
+    return render_template('user.html', name=name, current_time=datetime.utcnow())
 
 if __name__ == '__main__':
     app.run(debug=True)
