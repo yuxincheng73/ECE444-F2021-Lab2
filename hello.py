4165f99bc47aab8a0504cb7a9a4403eae1caf9f7
Yu Xin Cheng <yx.cheng73@gmail.com>
2021-09-25 23:42:52 -0400

    Added email field to form and logic to handle different email formats

--- hello.py
+++ hello.py
@@ -2,7 +2,9 @@ from flask import Flask, render_template, session, redirect, url_for, flash
 from datetime import datetime
 from flask_wtf import Form
 from wtforms import StringField, SubmitField
-from wtforms.validators import Required
+from wtforms import validators
+from wtforms.fields.html5 import EmailField
+from wtforms.validators import DataRequired, Email
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 
@@ -12,7 +14,8 @@ bootstrap = Bootstrap(app)
 moment = Moment(app)
 
 class NameForm(Form):
-    name = StringField('What is your name?', validators=[Required()])
+    name = StringField('What is your name?', validators=[DataRequired()])
+    email = EmailField('What is your UofT Email address', validators=[DataRequired(), Email()])
     submit = SubmitField('Submit')
 
 @app.route('/', methods=['GET', 'POST'])
@@ -21,11 +24,15 @@ def index():
     if form.validate_on_submit():
         session.permanent = False
         old_name = session.get('name')
+        old_email = session.get('email')
         if old_name is not None and old_name != form.name.data:
             flash('Looks like you have changed your name!')
+        if old_email is not None and old_email != form.email.data:
+            flash('Looks like you have changed your email!')
         session['name'] = form.name.data
+        session['email'] = form.email.data
         return redirect(url_for('index'))
-    return render_template('index.html', form=form, name=session.get('name'))
+    return render_template('index.html', form=form, name=session.get('name'), email=session.get('email'))
     
 @app.route('/user/<name>')
 def user(name):
